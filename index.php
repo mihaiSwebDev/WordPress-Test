45da4895083b87731cec69362f9a1342e872180c
Mihai Savescu <mihaiasavescu@gmail.com>
2021-11-24 19:43:38 +0200

    Initialized WordPress plugin

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Plugin Name: SWA Test Plugin
+Plugin URI: https://swissacademy.eu
+Description: This is a wordpress implementation of a demo plugin
+Author: Mihai Savescu
+Version: 0.1
+Author URI: https://swissacademy.eu
+*/
